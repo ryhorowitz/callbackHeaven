b56959c5760419443658828b81521f34910fa5c0
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-01-14 22:17:47 -0500

    init

---
+++ callbackHeaven.js
@@ -0,0 +1,46 @@
+// nothing should be edited below this line until the next comment marker
+function fastFunction(cb) {
+  setTimeout(function () {
+    cb("fastFunction error");
+  }, 100);
+}
+
+function slowFunction(cb) {
+  setTimeout(function () {
+    cb(null, "slowFunction worked");
+  }, 300);
+}
+
+function immediateFunction(cb) {
+  cb(null, "immediateFunction worked");
+}
+
+// nothing should be edited above this line
+
+function runSequentially(cb) {
+  // pass in `messageHandler` as the callback to this function
+  // invoke the 3 functions at the top of this file within this function
+  // your code here... 
+
+}
+
+function messageHandler(err, data) {
+  // use this function to define the callback that will be passed to `runSequentially`
+  // this function supplies the "messageHandler handled " message  
+  // your code here...
+  if (err) throw err;
+
+  return data;
+};
+
+// after defining everything, invoke like this:
+runSequentially(/* put something in here */);
+
+/*
+  expected output of invoking `runSequentially`:
+
+  "messageHandler handled slowFunction worked"
+  "messageHandler handled fastFunction error"
+  "messageHandler handled immediateFunction worked"
+
+*/
