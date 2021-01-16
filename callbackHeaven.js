c5ab9c94c1755f8d2b7fe4a9ec0eac5241d9c0ed
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-01-15 23:36:00 -0500

    Revert "init"
    This reverts commit b56959c5760419443658828b81521f34910fa5c0.

--- callbackHeaven.js
+++ callbackHeaven.js
@@ -1,45 +1,52 @@
-debugger;// nothing should be edited below this line until the next comment marker
-function fastFunction(cb) {
+ // nothing should be edited below this line until the next comment marker
+function fastFunction (cb) {
   setTimeout(function () {
-    cb("fastFunction error");
-  }, 100);
+    cb('fastFunction error')
+  }, 100)
 }
 
-function slowFunction(cb) {
+function slowFunction (cb) {
   setTimeout(function () {
-    cb(null, "slowFunction worked");
-  }, 300);
+    cb(null, 'slowFunction worked')
+  }, 300)
 }
 
-function immediateFunction(cb) {
-  cb(null, "immediateFunction worked");
+function immediateFunction (cb) {
+  cb(null, 'immediateFunction worked')
 }
-
 // nothing should be edited above this line
 
-function runSequentially(cb) {
-  cb(null, immediateFunction);
-  //fastFunction(cb);
-  //slowFunction(cb);
-
-}
+function runSequentially (cb) { 
   // pass in `messageHandler` as the callback to this function
   // invoke the 3 functions at the top of this file within this function
-  // your code here... 
-
-
-function messageHandler(err, data) {
-  // use this function to define the callback that will be passed to `runSequentially`
-  // this function supplies the "messageHandler handled " message  
   // your code here...
-  if (err) {`messagehandler handled ${err} error`};
+  // invoke immediateFunction( ( err, data ) => { 
+  immediateFunction((err, data) =>{
+    //if err what? console.error(`Error in messageHandler`, err)
+    if (err) {
+      console.error(`Error in immediateFunction`, err)
+      cb(err);
+    } else {
+      cb(null, data);
+    }
+  })
+    // 
+};
 
-  return `messagehandler handled ${data} worked`;  
+function messageHandler (err, data) {
+  // use this function to define the callback that will be passed to `runSequentially`
+  // this function supplies the "messageHandler handled " message
+  // your code here...
+  if (err) {
+    console.error(`Error in messageHandler`, err);
+    console.log(`messageHandler handled ${data} error`);
+  } else {
+    console.log(`messageHandler handled ${data}`);
+  }
 };
 
 // after defining everything, invoke like this:
-runSequentially(messageHandler(null, immediateFunction));//????
-
+runSequentially(messageHandler);
 /*
   expected output of invoking `runSequentially`:
 
