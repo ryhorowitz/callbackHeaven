e25309779e596850332857d7a8f151bbd9c2c397
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-08-03 18:37:45 -0400

    Reverted back to original.

--- callbackHeaven.js
+++ callbackHeaven.js
@@ -1,57 +1,44 @@
- // nothing should be edited below this line until the next comment marker
-function fastFunction (cb) {
+// nothing should be edited below this line until the next comment marker
+function fastFunction(cb) {
   setTimeout(function () {
-    cb('fastFunction error')
-  }, 100)
+    cb("fastFunction error");
+  }, 100);
 }
 
-function slowFunction (cb) {
+function slowFunction(cb) {
   setTimeout(function () {
-    cb(null, 'slowFunction worked')
-  }, 300)
+    cb(null, "slowFunction worked");
+  }, 300);
 }
 
-function immediateFunction (cb) {
-  cb(null, 'immediateFunction worked')
+function immediateFunction(cb) {
+  cb(null, "immediateFunction worked");
 }
+
 // nothing should be edited above this line
 
-function runSequentially (cb) { 
+function runSequentially(cb) {
   // pass in `messageHandler` as the callback to this function
   // invoke the 3 functions at the top of this file within this function
   // your code here...
-  // invoke immediateFunction( ( err, data ) => { 
-  immediateFunction((err, data) =>{
-    //if err what? console.error(`Error in messageHandler`, err)
-    if (err) {
-      console.error(`Error in immediateFunction`, err)
-      cb(err);
-    } else {
-      cb(null, data);
-    }
-  })
-    // 
-};
 
-function messageHandler (err, data) {
+}
+
+function messageHandler(err, data) {
   // use this function to define the callback that will be passed to `runSequentially`
   // this function supplies the "messageHandler handled " message
   // your code here...
-  if (err) {
-    console.error(`Error in messageHandler`, err);
-    console.log(`messageHandler handled ${data} error`);
-  } else {
-    console.log(`messageHandler handled ${data}`);
-  }
+  if (err) throw err;
+
+  return data;
 };
 
 // after defining everything, invoke like this:
-runSequentially(messageHandler);
+runSequentially(/* put something in here */);
+
 /*
   expected output of invoking `runSequentially`:
-
   "messageHandler handled slowFunction worked"
   "messageHandler handled fastFunction error"
   "messageHandler handled immediateFunction worked"
-
 */
